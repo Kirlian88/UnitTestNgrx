a993214428552b27b2a320514611b725838e3ff1
Kirlian88 <kirlian17@gmail.com>
2019-12-31 10:41:39 -0500

    Add files via upload

--- src/app/blockchain/blockchain.component.spec.ts
+++ src/app/blockchain/blockchain.component.spec.ts
@@ -5,23 +5,37 @@ import { DebugElement } from '@angular/core';
 
 import { BlockchainComponent } from './blockchain.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { StoreModule, Store } from '@ngrx/store';
+import { Store } from '@ngrx/store';
+import { Blockchain } from './blockchain.model';
+import { ADD_product } from '../reducers/blockchain.reducer';
+import { AppState } from '../app.state';
+import { of } from 'rxjs';
 
 describe('BlockchainComponent', () => {
   let component: BlockchainComponent;
   let fixture: ComponentFixture<BlockchainComponent>;
 
+
   let product: DebugElement;
   let price: DebugElement;
   let btnadd: DebugElement;
+
+  const storeMock = {
+    select() {
+        return of([{ name: 'water', price: '120000' },
+        { name: 'coil', price: '85000' }]);
+    },
+    dispatch: () => null
+}
+
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ BlockchainComponent ],
       imports: [FormsModule, ReactiveFormsModule],
       providers: [
-        { provide: Store}, 
-      ],
+        { provide: Store, useValue: storeMock }, 
+      ]
     })
     .compileComponents();
   }));
@@ -30,7 +44,7 @@ describe('BlockchainComponent', () => {
     fixture = TestBed.createComponent(BlockchainComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-
+    
 
     btnadd = fixture.debugElement.query(By.css('button[id=btnadd]'));
     price = fixture.debugElement.query(By.css('input[id=price]'));
@@ -52,5 +66,21 @@ describe('BlockchainComponent', () => {
   it('exists input price' , () => {
     expect(price).toBeTruthy();
   });
+
+  it('should to find the method', async(() => {
+    var myspy = spyOn(component, 'addproduct').and.callThrough();
+
+    fixture.whenStable().then(() => {
+      expect(myspy).toBeDefined();
+    });
+  }));
+
+
+  it('should dispatch the select scenario action', () => {
+    let store: Store<AppState>;
+
+    component.addproduct('tool', 345000);
+    expect(storeMock.dispatch);
+});
 
 });
